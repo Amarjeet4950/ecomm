b135a68806f850b2bc1632659800e30364aaeafd
Amarjeet4950 <67182151+Amarjeet4950@users.noreply.github.com>
2022-02-08 14:17:48 +0530

    Update Auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -1,7 +1,8 @@
 
 import { Container , Box , Typography ,Button,Grid,Link, TextField, FormControlLabel,Avatar , Checkbox } from '@mui/material';
-export default function Login() {
 
+
+export default function Login() {
   const handleSubmit = () =>{
 
   }
@@ -71,4 +72,93 @@ export default function Login() {
   );
   }
   
+  
+  
+import {Button,Container , Box ,Typography, Grid , TextField,FormControlLabel,Checkbox,Link,} from '@mui/material';
+
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+      <Container component="main" maxWidth="xs">
+        
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="Name"
+                  required
+                  fullWidth
+                  id="Name"
+                  label="Your Name"
+                  autoFocus
+                />
+              </Grid>
+              
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+  
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+
+  );
+}
 
